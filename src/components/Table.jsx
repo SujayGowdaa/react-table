5217e48364f4e2b8452eb91a60ec27823ffa29c3
SUJAY GOWDA <sujay.gowda@i4sight.net>
2024-04-10 13:49:30 +0530

    add table row and data

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,4 +1,8 @@
-import { useReactTable, flexRender } from '@tanstack/react-table';
+import {
+  useReactTable,
+  flexRender,
+  getCoreRowModel,
+} from '@tanstack/react-table';
 import { tableData } from '../data/data';
 import { columnsDef } from '../components/columns';
 import React from 'react';
@@ -14,6 +18,7 @@ export default function Table() {
   const table = useReactTable({
     columns: column, // Define the columns for the table using the columnsDef array
     data: data, // Provide the data to be displayed in the table
+    getCoreRowModel: getCoreRowModel(),
   });
 
   return (
@@ -39,6 +44,26 @@ export default function Table() {
             );
           })}
         </thead>
+        <tbody>
+          {table.getCoreRowModel().rows.map((row) => {
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((row) => {
+                  return (
+                    <td key={row.id}>
+                      {flexRender(
+                        // Render the header of the column using the flexRender function
+                        row.column.columnDef.cell,
+                        // Pass the context of the column to the flexRender function for rendering
+                        row.getContext()
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
